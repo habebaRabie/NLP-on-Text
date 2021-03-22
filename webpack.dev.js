05aa2c0da45c807b12e6bc645a18f1cbe94cbb90
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2021-03-22 18:16:59 +0200

    Remove quotes

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
@@ -13,33 +13,32 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
-                test: '/\.scss$/',
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                test: /\.scss$/,
+                use: [
+                    {loader: MiniCssExtractPlugin.loader}, 
+                    'css-loader', 'sass-loader']
             }
         ]
     },
     plugins: [
+        new MiniCssExtractPlugin({ filename:'main.css' }),
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        // new CleanWebpackPlugin({
-        //     // Simulate the removal of files
-        //     dry: true,
-        //     // Write Logs to Console
-        //     verbose: true,
-        //     // Automatically remove all unused webpack assets on rebuild
-        //     cleanStaleWebpackAssets: true,
-        //     protectWebpackAssets: false,
-        //     cleanOnceBeforeBuildPatterns: ['./dist/*']
-        // }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        })
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false,
+        }),
     ]
 }
